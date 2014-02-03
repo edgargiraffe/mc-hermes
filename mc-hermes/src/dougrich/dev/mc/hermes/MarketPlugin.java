b437c2785a97eab8726cd158965eef5a8cd8bc73
Douglas Richardson <doug-rich@outlook.com>
2014-02-03 14:24:43 -0800

    -Added lines to properly handle "shop" command

--- mc-hermes/src/dougrich/dev/mc/hermes/MarketPlugin.java
+++ mc-hermes/src/dougrich/dev/mc/hermes/MarketPlugin.java
@@ -2,10 +2,14 @@ package dougrich.dev.mc.hermes;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import dougrich.dev.mc.hermes.commands.ShopBaseCommandExecutor;
+
 public class MarketPlugin extends JavaPlugin {
 
 	public void onEnable() {
 		this.getLogger().info("Hermes Market Plugin Enabled");
+		
+		this.getCommand("shop").setExecutor(new ShopBaseCommandExecutor());
 	}
 	
 	public void onDisable() {
